6eff9d9202ceea63be785ff61aab0674112c4bbc
messat <148489126+messat@users.noreply.github.com>
2024-05-31 11:04:20 +0100

    Challenge 11: GET request using query string to populate filtered database

--- models/api.model.js
+++ models/api.model.js
@@ -28,11 +28,20 @@ exports.selectArticleById = (article_id)=>{
    })
 }
 
-exports.selectAllArticles = ()=>{
-    return db.query('SELECT articles.author, articles.title, articles.article_id, articles.topic,articles.created_at,articles.votes,articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;')
-    .then((data)=>{
-        return data.rows
-    })
+exports.selectAllArticles = (topic)=>{
+    if(topic){
+        let sqlString = `SELECT * FROM articles WHERE topic = $1;`
+        return db.query(sqlString, [topic])
+        .then((data)=>{
+            return data.rows
+        })
+    }
+    else {
+        return db.query('SELECT articles.author, articles.title, articles.article_id, articles.topic,articles.created_at,articles.votes,articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;')
+        .then((data)=>{
+            return data.rows
+        })
+    }
 }
 
 exports.selectCommentsByArticleId =  (article_id)=>{
